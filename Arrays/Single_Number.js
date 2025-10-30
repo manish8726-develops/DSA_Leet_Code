9a1d9b34d6750e7e94fc6891af27267885dc932a
Manish <manish8726370307@gmail.com>
2025-10-30 20:52:25 +0530

    single number

--- Arrays/Single_Number.js
+++ Arrays/Single_Number.js
@@ -1,11 +1,12 @@
-var singleNumber = function(nums=[4,1,2,1,2]) {
-    for (let i = 0; i < nums.length; i++) {
-     for (let j = 0; j < nums.length; j++) {
-      if (nums[i]===nums[j]) {
-        return nums[j]
-      }
-     }
+// var singleNumber = function(nums=[4,1,2,1,2]) {
+//     for (let i = 0; i < nums.length; i++) {
+//      for (let j = 0; j < nums.length; j++) {
+//       if (nums[i]===nums[j]) {
+//         return nums[j]
+            
+//       }
+//      }
       
-    }
-};
-singleNumber()
+//     }
+// };
+// singleNumber()
